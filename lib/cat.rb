5389e0fc1cd497aaf898ad0b358bb026fa4dafde
Yakura Marzett <marzettyakura@yahoo.com>
2020-04-17 23:02:43 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,10 @@
 class Cat
   attr_reader :name
   attr_accessor :mood, :owner
-  @@cats []
+  @@cats =[]
 
   def initialize(name, mood = "nervous")
     @mood = mood
-    @name = name
+    @name = name , owner, mood
   end
 end
