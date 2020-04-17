6feb0cd0b550695d35be8dcdc6284e7bd89bc043
Yakura Marzett <marzettyakura@yahoo.com>
2020-04-17 23:17:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,60 +1,69 @@
 class Owner
-  attr_accessor :pets, :name
-  attr_reader :species
-
+  
+  attr_accessor :pets
+  attr_reader :name, :species, :fish, :dog, :cat
   @@all = []
-
-  def initialize(species)
-    @species = species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@pets = {:fish => [], :dogs => [], :cats => []}
     @@all << self
-    @pets = {:dogs => [], :cats => [] }
   end
-
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+  
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs 
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  
+  def walk_dogs
+    dogs.each {|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats
+    cats.each {|cat| cat.mood = "happy"}
+  end
+  
+  def sell_pets
+    cats.each do |cat| 
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end
+  
+  def list_pets
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
+  
+  
   def self.all
     @@all
   end
 
   def self.reset_all
-    @@all.clear
+    self.all.clear
   end
 
   def self.count
-    @@all.size
+    self.all.size
   end
-
-  def say_species
-    "I am a #{species}."
-  end
-
-
-  def buy_cat(name)
-    cat = Cat.new(name)
-    pets[:cats] << cat
-  end
-
-  def buy_dog(name)
-    dog = Dog.new(name)
-    pets[:dogs] << dog
-  end
-
-  def walk_dogs
-    pets[:dogs].map {|dog| dog.mood = "happy"}
-
-  end
-
-  def play_with_cats
-    pets[:cats].map {|cat| cat.mood = "happy"}
-  end
-
-  def sell_pets
-    pets.each do |type, pets|
-      pets.map {|pet| pet.mood = "nervous"}
-    end
-    pets.clear
-  end
-
-  def list_pets
-    "I have #{pets[:dogs].size} dog(s) and #{pets[:cats].size} cat(s)."
-  end
-
 end
